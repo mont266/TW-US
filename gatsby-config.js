64de4d1fdfc72b8e6f2149fd7359d2e8511d119b
mont266 <scottmontford@gmail.com>
2022-10-04 23:10:42 +0100

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,82 @@
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    require('dotenv').config({ path: './.env.development'})
+}
+  
+if (process.env.NODE_ENV === 'production' || !process.env.NODE_ENV) {
+    require('dotenv').config({ path: './.env.production' })
+}
+
+module.exports = {
+    siteMetadata: {
+        title: `Talisman Webs -- Digital presence starts here.`,
+        description: `Your digital presence starts here. We build stunning websites at affordable prices to help kickstart the digital presence of small businesses in the UK.`,
+        author: `@envy_theme`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+              // The property ID; the tracking code won't be generated without it
+              trackingId: "G-4KNDMPXWV0",
+              // Defines where to place the tracking script - `true` in the head and `false` in the body
+              head: false,
+              // Setting this parameter is optional
+              anonymize: true,
+              // Setting this parameter is also optional
+              respectDNT: true,
+              // Avoids sending pageview hits from custom paths
+              exclude: ["/preview/**", "/do-not-track/me/too/"],
+              // Delays sending pageview hits on route update (in milliseconds)
+              pageTransitionDelay: 0,
+              // Enables Google Optimize using your container Id
+              optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+              // Enables Google Optimize Experiment ID
+              experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+              // Set Variation ID. 0 for original 1,2,3....
+              variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+              // Defers execution of google analytics script after page load
+              defer: false,
+              // Any additional optional fields
+              sampleRate: 5,
+              siteSpeedSampleRate: 10,
+              cookieDomain: "example.com",
+              // defaults to false
+              enableWebVitalsTracking: true,
+            },
+          },
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+                accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+            },
+        },
+        `gatsby-plugin-gatsby-cloud`,
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ],
+}
